7aef8bbdd0353b4f87f8ea4abe16605ef7cf55de
farizibnu <fariz2240@gmail.com>
2024-05-22 13:20:46 +0700

    fix jakarta implementation

--- paymentservice/src/main/java/com/pplbo/ecommerce/paymentservice/model/Payment.java
+++ paymentservice/src/main/java/com/pplbo/ecommerce/paymentservice/model/Payment.java
@@ -1,7 +1,7 @@
 package com.pplbo.ecommerce.paymentservice.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
--- paymentservice/src/main/java/com/pplbo/ecommerce/paymentservice/repository/PaymentRepository.java
+++ paymentservice/src/main/java/com/pplbo/ecommerce/paymentservice/repository/PaymentRepository.java
@@ -1,6 +1,6 @@
 package com.pplbo.ecommerce.paymentservice.repository;
 
-import com.example.paymentservice.model.Payment;
+import com.pplbo.ecommerce.paymentservice.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, String> {
