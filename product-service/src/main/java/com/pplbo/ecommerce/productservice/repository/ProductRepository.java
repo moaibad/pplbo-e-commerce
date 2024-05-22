2b1a12abd54da5ffb597336c9efdb623858d2808
moaibad <fathulibad.412@gmail.com>
2024-05-23 01:49:24 +0700

    add Product

--- product-service/src/main/java/com/pplbo/ecommerce/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/pplbo/ecommerce/productservice/repository/ProductRepository.java
@@ -1,5 +1,8 @@
 package com.pplbo.ecommerce.productservice.repository;
 
-public class ProductRepository {
+import com.pplbo.ecommerce.productservice.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
     
 }
