b7ae362ada9c79adab790b3818fed4446538e2e7
Al Ghani <alghani63@gmail.com>
2024-05-23 08:44:30 +0700

    add crud for order version 1

---
+++ order-service/src/main/java/com/pplbo/orderservice/model/Shipping.java
@@ -0,0 +1,25 @@
+package com.pplbo.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "shippings")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Shipping {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long shippingId;
+    
+    private String shippingName;
+    private Double shippingPrice;
+    private String shippingStatus;
+    private String shippingAddress;
+}
