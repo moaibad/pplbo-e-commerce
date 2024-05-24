d9b5ac255154fad5ff530b1d0aab26f57f41197d
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-05-25 01:51:32 +0700

    update brand and category

--- product-service/src/main/java/com/pplbo/ecommerce/productservice/repository/BrandRepository.java
+++ product-service/src/main/java/com/pplbo/ecommerce/productservice/repository/BrandRepository.java
@@ -4,4 +4,7 @@ import com.pplbo.ecommerce.productservice.model.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
+    Brand findByName(String name);
+    Brand findById(int id);
+    void deleteById(int id);
 }
