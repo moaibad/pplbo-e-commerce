910b6de990fceab89ced1c3e42ad4a5164ba92ac
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-05-24 20:18:39 +0700

    Update CRUD on Brand

--- product-service/src/main/java/com/pplbo/ecommerce/productservice/controller/BrandController.java
+++ product-service/src/main/java/com/pplbo/ecommerce/productservice/controller/BrandController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/brand")
 @RequiredArgsConstructor
@@ -18,5 +20,32 @@ public class BrandController {
     @ResponseStatus(HttpStatus.CREATED)
     public Brand createBrand(@RequestBody BrandRequest brandRequest){
         return brandService.createBrand(brandRequest);
+    }
+
+    @GetMapping("/{name}")
+    public Brand getBrandByName(@PathVariable String name){
+        return brandService.getBrandByName(name);
+    }
+
+    @GetMapping("/{id}")
+    public Brand getBrandById(@PathVariable Integer id){
+        return brandService.getBrandById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeBrand(@PathVariable Integer id){
+        brandService.removeBrand(id);
+    }
+
+    //change brand name
+    @PutMapping("/{id}")
+    public Brand updateBrand(@PathVariable Integer id, @RequestBody BrandRequest brandRequest){
+        return brandService.updateBrand(id, brandRequest);
+    }
+
+    @GetMapping("/allbrand")
+    public List<Brand> getAllBrands(){
+        return brandService.getAllBrands();
     }
 }
