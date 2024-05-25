32afa5d59d0fa50bd2582ee3972742419b159c03
moaibad <fathulibad.412@gmail.com>
2024-05-26 06:50:40 +0700

    add getAllProducts service

--- product-service/src/main/java/com/pplbo/ecommerce/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/pplbo/ecommerce/productservice/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.pplbo.ecommerce.productservice.controller;
 
 import com.pplbo.ecommerce.productservice.dto.ProductRequest;
+import com.pplbo.ecommerce.productservice.dto.ProductResponse;
 import com.pplbo.ecommerce.productservice.model.Product;
 import com.pplbo.ecommerce.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -16,7 +19,13 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Product createProduct(@RequestBody ProductRequest productRequest){
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest){
         return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
     }
 }
