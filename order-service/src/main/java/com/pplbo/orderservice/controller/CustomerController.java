9acb3ae860310cfb96c8e1fcb3687d4addd4c3d7
fardantif21 <fardan.al.tif421@polban.ac.id>
2024-05-29 16:55:21 +0700

    add endpoint to get customer by ID

--- order-service/src/main/java/com/pplbo/orderservice/controller/CustomerController.java
+++ order-service/src/main/java/com/pplbo/orderservice/controller/CustomerController.java
@@ -6,13 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.pplbo.orderservice.service.CustomerService;
 import com.pplbo.orderservice.dto.CustomerRequest;
+import com.pplbo.orderservice.dto.CustomerResponse;
 
 
 @RestController
@@ -28,6 +34,18 @@ public class CustomerController {
   public String createCustomer(@RequestBody CustomerRequest customerRequest) {
     customerService.createCustomer(customerRequest);
     return "Customer created successfully";
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<CustomerResponse> getCustomerById(@PathVariable Long id) {
+    Optional<CustomerResponse> customerResponseOptional = customerService.findById(id);
+
+    if (customerResponseOptional.isPresent()) {
+      CustomerResponse customerResponse = customerResponseOptional.get();
+      return ResponseEntity.ok(customerResponse);
+    } else {
+      return ResponseEntity.notFound().build();
+    }
   }
 
 }
--- order-service/src/main/java/com/pplbo/orderservice/service/CustomerService.java
+++ order-service/src/main/java/com/pplbo/orderservice/service/CustomerService.java
@@ -1,8 +1,11 @@
 package com.pplbo.orderservice.service;
 
 import com.pplbo.orderservice.dto.CustomerRequest;
+import com.pplbo.orderservice.dto.CustomerResponse;
 import com.pplbo.orderservice.model.Customer;
 import com.pplbo.orderservice.repository.CustomerRepository;
+
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,4 +25,17 @@ public class CustomerService {
 
     customerRepository.save(customer);
   }
+
+  public Optional<CustomerResponse> findById(Long id) {
+    Optional<Customer> customerOptional = customerRepository.findById(id);
+    
+    if (customerOptional.isPresent()) {
+      Customer customer = customerOptional.get();
+      CustomerResponse customerResponse = new CustomerResponse(customer.getCustomerId(), customer.getFirstName(), customer.getLastName());
+      return Optional.of(customerResponse);
+    } else {
+      return Optional.empty();
+    }
+  }
+
 }
