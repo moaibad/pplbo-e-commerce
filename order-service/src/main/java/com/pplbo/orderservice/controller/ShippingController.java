9b2092bb15cace030b612e5613f67aeca758d025
fardantif21 <fardan.al.tif421@polban.ac.id>
2024-05-29 17:40:57 +0700

    tertinggal

--- order-service/src/main/java/com/pplbo/orderservice/controller/ShippingController.java
+++ order-service/src/main/java/com/pplbo/orderservice/controller/ShippingController.java
@@ -1,5 +1,7 @@
 package com.pplbo.orderservice.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,10 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+
+import com.pplbo.orderservice.service.ShippingService;
+import com.pplbo.orderservice.dto.ShippingRequest;
+import com.pplbo.orderservice.dto.ShippingResponse;
 
 @RestController
 @RequestMapping("/api/shipping")
--- order-service/src/main/java/com/pplbo/orderservice/service/ShippingService.java
+++ order-service/src/main/java/com/pplbo/orderservice/service/ShippingService.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
+import com.pplbo.orderservice.dto.ShippingRequest;
+import com.pplbo.orderservice.dto.ShippingResponse;
+import com.pplbo.orderservice.model.Shipping;
+import com.pplbo.orderservice.repository.ShippingRepository;
+
 @Service
 @RequiredArgsConstructor
 public class ShippingService {
@@ -16,10 +21,10 @@ public class ShippingService {
 
   public void createShipping(ShippingRequest shippingRequest) {
     Shipping shipping = new Shipping();
-    shipping.setShippingName(shippingRequest.getShippingName());
-    shipping.setShippingPrice(shippingRequest.getShippingPrice());
-    shipping.setShippingStatus(shippingRequest.getShippingStatus());
-    shipping.setShippingAddress(shippingRequest.getShippingAddress());
+    shipping.setShippingName(shippingRequest.shippingName());
+    shipping.setShippingPrice(shippingRequest.shippingPrice());
+    shipping.setShippingStatus(shippingRequest.shippingStatus());
+    shipping.setShippingAddress(shippingRequest.shippingAddress());
 
     shippingRepository.save(shipping);
   }
@@ -29,8 +34,7 @@ public class ShippingService {
 
     if (shippingOptional.isPresent()) {
       Shipping shipping = shippingOptional.get();
-      ShippingResponse shippingResponse = new ShippingResponse(shipping.getShippingName(), 
-        shipping.getShippingPrice(), shipping.getShippingStatus(), shipping.getShippingAddress());
+      ShippingResponse shippingResponse = new ShippingResponse(shipping.getShippingId(), shipping.getShippingName(), shipping.getShippingPrice(), shipping.getShippingStatus(), shipping.getShippingAddress());
       return Optional.of(shippingResponse);
     }
     else {
