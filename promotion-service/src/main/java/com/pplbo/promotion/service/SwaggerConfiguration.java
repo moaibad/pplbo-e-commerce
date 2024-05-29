6fbfe478d0840920a38a7356de5c69acec05a853
Berliana Elfada <berlianaelfd23@gmail.com>
2024-05-29 22:24:36 +0700

    add swagger ui

---
+++ promotion-service/src/main/java/com/pplbo/promotion/service/SwaggerConfiguration.java
@@ -0,0 +1,21 @@
+package com.pplbo.promotion.service;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI defineOpenApi() {
+ 
+        Info information = new Info()
+                .title("Promotions Service API")
+                .version("1.0")
+                .description("List API for Promotions Service.");
+
+        return new OpenAPI().info(information);
+    }
+}
