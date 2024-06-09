9fcd82dceb1df5b83e6bb596db6a163c0594a3ba
moaibad <fathulibad.412@gmail.com>
2024-06-09 19:22:59 +0700

    update base api for BrandController

--- product-service/src/main/java/com/pplbo/ecommerce/productservice/controller/BrandController.java
+++ product-service/src/main/java/com/pplbo/ecommerce/productservice/controller/BrandController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/products/brand")
+@RequestMapping("/api/product/brand")
 @RequiredArgsConstructor
 public class BrandController {
 
