40a51bf49f04e66acd8b25de0925f3d5f967b859
RaihanFuadSyakir <142875762+RaihanFuadSyakir@users.noreply.github.com>
2024-06-10 10:07:44 +0700

    fixed logics

--- cart-service/app/src/main/java/com/pplbo/ecommerce/cart/service/controller/ProductController.java
+++ cart-service/app/src/main/java/com/pplbo/ecommerce/cart/service/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.pplbo.ecommerce.cart.service.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,38 +12,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 
-import com.pplbo.ecommerce.cart.service.dto.CartProductRequest;
-import com.pplbo.ecommerce.cart.service.dto.CartRequest;
-import com.pplbo.ecommerce.cart.service.dto.ProductRequest;
-import com.pplbo.ecommerce.cart.service.dto.ProductResponse;
 import com.pplbo.ecommerce.cart.service.error.ResourceNotFoundException;
-import com.pplbo.ecommerce.cart.service.model.Cart;
 import com.pplbo.ecommerce.cart.service.model.Product;
-import com.pplbo.ecommerce.cart.service.service.CartService;
 import com.pplbo.ecommerce.cart.service.service.ProductService;
 
-import lombok.RequiredArgsConstructor;
-
-import java.util.List;
-import java.util.Optional;
-
 @RestController
-@RequestMapping("/api/carts")
-public class CartController {
+@RequestMapping("/api/products")
+public class ProductController {
 
     @Autowired
-    private CartService cartService;
-    @Autowired
-    ProductService productService;
+    private ProductService productService;
 
     @GetMapping
-    public ResponseEntity<List<Cart>> getAllCarts() {
-        List<Cart> carts = cartService.getAllCarts();
-        return ResponseEntity.ok(carts);
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
     }
 
     @GetMapping("/{id}")
@@ -57,19 +46,19 @@ public class CartController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cart createCart(@RequestBody Cart cart) {
-        return cartService.createCart(cart);
+    public Product createProduct(@RequestBody Product product) {
+        System.out.println("product added to db");
+        return productService.createProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return productService.updateProduct(id, product);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteCart(@PathVariable Long id) {
-        cartService.deleteCart(id);
-    }
-
-    @PostMapping("/{cartId}/products/{productId}")
-    public void addProductToCart(@PathVariable Long cartId, @PathVariable Long productId,
-            @RequestParam Integer quantityToBuy) {
-        cartService.addProductToCart(cartId, productId, quantityToBuy);
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
     }
 }
