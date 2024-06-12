a8f3df43b94b48f92d25138c9d7200ac8e85e80a
moaibad <fathulibad.412@gmail.com>
2024-06-12 09:09:49 +0700

    add stock parameter

--- product-service/src/main/java/com/pplbo/ecommerce/productservice/dto/ProductDetailResponse.java
+++ product-service/src/main/java/com/pplbo/ecommerce/productservice/dto/ProductDetailResponse.java
@@ -2,6 +2,6 @@ package com.pplbo.ecommerce.productservice.dto;
 
 import java.util.List;
 
-public record ProductDetailResponse(Integer id, String name, Long price, String description, String brand, String image, List<String> category)  {
+public record ProductDetailResponse(Integer id, String name, Long price, String description, String brand, String image, List<String> category, Integer stock)  {
 
 }
